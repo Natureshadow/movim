7b884519b94b169a57fae44a4a334784281d7d01
mirabilos <thorsten.glaser@teckids.org>
2019-05-22 18:48:02 +0200

    implement RFC 3986 IP-literal/IPv4address matching required by RFC 6122

--- app/helpers/StringHelper.php
+++ app/helpers/StringHelper.php
@@ -441,7 +441,40 @@ function prepJid($value) {
     }
 
     // Finally, validate domain part
-    $domain = prepJidPart($rest, new Nameprep());
+    // RFC 6122: IP-literal or IPv4address of RFC 3986…
+    if (($domain = preg_match('/
+        (?(DEFINE)
+            (?<h16> [0-9A-Fa-f]{1,4} )
+            (?<ls32> (?: (?&h16) : (?&h16) | (?&IPv4address) ) )
+            (?<IPv6address> (?:
+                                                      (?: (?&h16) : ){6} (?&ls32) |
+                                                   :: (?: (?&h16) : ){5} (?&ls32) |
+                (?:                     (?&h16) )? :: (?: (?&h16) : ){4} (?&ls32) |
+                (?: (?: (?&h16) :){0,1} (?&h16) )? :: (?: (?&h16) : ){3} (?&ls32) |
+                (?: (?: (?&h16) :){0,2} (?&h16) )? :: (?: (?&h16) : ){2} (?&ls32) |
+                (?: (?: (?&h16) :){0,3} (?&h16) )? ::     (?&h16) :      (?&ls32) |
+                (?: (?: (?&h16) :){0,4} (?&h16) )? ::                    (?&ls32) |
+                (?: (?: (?&h16) :){0,5} (?&h16) )? ::                    (?&h16) |
+                (?: (?: (?&h16) :){0,6} (?&h16) )? :: )
+            )
+            (?<sub_delims> [!$&' . "'" . '()*+,;=] )
+            (?<unreserved> [A-Za-z0-9._~-] )
+            (?<IPvFuture> v [0-9A-Fa-f]+ \. (?: (?&unreserved) | (?&sub_delims) | : ) )
+            (?<IP_literal> \[ (?: (?&IPv6address) | (?&IPvFuture) ) \] )
+            (?<dec_octet> [0-9] | [1-9][0-9] | 2[0-4][0-9] | 25[0-5] )
+            (?<IPv4address> (?&dec_octet) \. (?&dec_octet) \. (?&dec_octet) \. (?&dec_octet) )
+        )
+        ^(?: (?&IP_literal) | (?&IPv4address) )$
+      /x', $rest)) === false) {
+        throw new \Exception('Internal pcre error: ' . preg_last_error());
+    } elseif ($domain /* regexp matched */) {
+echo "D: <$rest> matches IP-literal or IPv4address\n";
+        $domain = $rest;
+    } else {
+        // … or satisfying the Nameprep profile of stringprep
+        $domain = prepJidPart($rest, new Nameprep());
+echo "D: <$rest> " . ($domain ? "matches" : "does not match") . " <$domain>\n";
+    }
 
     // Invalid if any part is invalid
     if (($local === false) || ($domain === false) || ($resource === false)) {
